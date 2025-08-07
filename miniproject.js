d260dcc9e028f900de8d142465882b31db5c8710
NEVIL <nevilj22@gmail.com>
2025-08-07 19:34:12 +0530

    letvariable.js

---
+++ miniproject.js
@@ -0,0 +1,14 @@
+const restaurantName = "Annapoorna";
+const tableNumber = 12;
+
+let order = "Dosa";
+order = "Poori"; 
+
+let totalAmount = 10;
+let price = 10;
+totalAmount = price + (price * 1); 
+
+console.log(`Welcome to ${restaurantName}`);
+console.log(`Table No: ${tableNumber}`);
+console.log(`Your Order: ${order}`);
+console.log(`Total Bill: ₹${totalAmount}`);
