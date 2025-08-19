73199b579affbd46424c94384f5b09c6e8a6c790
NEVIL <nevilj22@gmail.com>
2025-08-19 21:50:58 +0530

    excerisefunction1.js

---
+++ excerisefunction1.js
@@ -0,0 +1,11 @@
+var favouriteactor = "vijay";
+var favouriteplayer = "Rohit Sharma";
+var favouritemovie = "leo";
+
+function favourite() {
+    console.log("My favourite actor is " + favouriteactor);
+    console.log("My favourite player is " + favouriteplayer);
+    console.log("My favourite movie is " + favouritemovie);
+}
+
+favourite();
