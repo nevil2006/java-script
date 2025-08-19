5b47b53af6ce1d246d46c406cf9098f2e971b15d
NEVIL <nevilj22@gmail.com>
2025-08-19 20:47:44 +0530

    functions.js

---
+++ functions.js
@@ -0,0 +1,30 @@
+function XYZ()
+{
+    console.log("HI NEVIL , HOW ARE YOU?");
+    console.log("I AM FINE, THANK YOU!");
+}
+
+XYZ()
+
+function iphone()
+{
+    console.log("I is iphone");
+}
+iphone()
+
+function redmi()
+{
+    console.log("I is redmi");
+}
+redmi()
+
+function oneplus()
+{
+    console.log("I is oneplus");
+}
+function oppo()
+{
+    console.log("I is oppo");
+}
+oneplus()
+oppo()
