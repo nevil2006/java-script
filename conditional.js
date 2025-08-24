dd52594a31efdbb81f820b6f89560c909e9f216b
NEVIL <nevilj22@gmail.com>
2025-08-24 20:02:08 +0530

    conditional.JS

---
+++ conditional.js
@@ -0,0 +1,46 @@
+var rain= false;
+if(rain)
+{
+    console.log("take a raincoat");
+}
+else
+{
+    console.log("no need to take a raincoat");
+}
+
+var homework = true;
+if(homework)
+{
+    console.log("Great job!");
+}
+else
+{
+    console.lof("Finish your homework before playing");
+}
+var cookies = true;
+if(cookies)
+{
+    console.log("eat the cookies");
+}
+else
+{
+    console.log("time to bake more cookies");
+}
+var brush = true;
+if(brush)
+{
+    console.log("brush your teeth");
+}
+else
+{
+    console.log("go brush your teeth");
+}
+var birthday = false;
+if(birthday)
+{
+    console.log("happy birthday");
+}   
+else
+{
+    console.log("sorry not a birthday");
+}
