1f2afc01ca376f9fe490cb35119ee729c4483d34
NEVIL <nevilj22@gmail.com>
2025-08-24 21:14:41 +0530

    excerise3.JS

---
+++ excerise3.js
@@ -0,0 +1,17 @@
+var score=100;
+if (score>50)
+{
+    console.log("You passed the exam");
+}
+else
+{
+    console.log("You failed the exam");
+}
+if (score>80 && score<=100)
+{
+    console.log("You got an A");
+}
+if(score>70)
+{
+    console.log("You got a B");
+}
