f7891d4516ff73ded640b940b3e7eb22ccde0f5f
NEVIL <nevilj22@gmail.com>
2025-08-24 21:16:59 +0530

    excerise3.JS

--- excerise3.js
+++ excerise3.js
@@ -15,3 +15,19 @@ if(score>70)
 {
     console.log("You got a B");
 }
+let number = 7;
+
+if (number % 2 === 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
+let char = "m";
+
+char = char.toLowerCase();
+
+if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+    console.log("It's a vowel.");
+} else {
+    console.log("It's a consonant.");
+}
