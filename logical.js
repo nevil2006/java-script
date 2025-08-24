e8ab62127060ccf909b6dd62ee6cc52a3560890b
NEVIL <nevilj22@gmail.com>
2025-08-24 20:14:06 +0530

    excerise2.JS

---
+++ logical.js
@@ -0,0 +1,13 @@
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+console.log("---------------");
+console.log(true || true);
+console.log(true || false); 
+console.log(false || true);
+console.log(false || false);
+console.log("---------------"); 
+console.log(!true);
+console.log(!false);        
+console.log("---------------");
