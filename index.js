b2bd3e3fa262da94309a9a65ce31fd20d9db7a23
NEVIL <147979766+nevil2006@users.noreply.github.com>
2025-11-09 18:36:40 +0530

    local updates

--- index.js
+++ index.js
@@ -2,3 +2,31 @@ let uName="NEVIL"
 
 console.log(uName)
 
+var age=30;
+
+var age; //declaration
+
+age=40; //initialization
+
+var age=70;
+age="Forty Five"
+console.log(age)
+
+//var
+var a;        // declaration
+a = 5;        // initialization
+a = 10;       // re-initialization (allowed)
+
+//let
+let b;        // declaration
+b = 8;        // initialization
+b = 15;       // re-initialization (allowed)
+
+//const
+const c = 20; // declaration + initialization required at same time
+// c = 25;    // re-initialization NOT allowed (Error)
+
+console.log("Hello");        // Normal output
+console.error("Something went wrong"); // Error message
+console.warn("Be careful"); // Warning message
+console.clear(); // Clears everything
